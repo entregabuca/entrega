390bb7c0272271154cf0f1221440a724161c426b
Mario Gamez <mg.dev.co@gmail.com>
2019-03-04 21:50:07 -0600

    Can now update and create a new Company with an address

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < ApplicationRecord
   has_many :transporters, dependent: :destroy
   has_many :orders, through: :transporters
-  has_many :locations, as: :addressable
+  has_many :locations, as: :addressable, :dependent => :destroy
 
   accepts_nested_attributes_for :locations, reject_if: :all_blank, allow_destroy: true
 
