27986ed892056230f4a7dbd2d8087dd97c4b0e24
Mario Gamez <mg.dev.co@gmail.com>
2019-03-24 22:59:46 -0600

    Map to pickup coordinates in Company

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,70 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+
+
+
+function writeCoordinates(){
+  if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function (position) {
+	  	var lat = position.coords.latitude,
+					lng = position.coords.longitude;
+			$('#latitude').val(lat);
+			$('#longitude').val(lng);
+			map.setView([lat, lng], 13);
+			marker.setLatLng(map.getCenter());
+	  });
+	} else {
+	  alert("Geolocation is not supported by this browser! Upgrade!");
+	}
+	iniMap(map); 
+  event.preventDefault();
+
+}
+
+function getCenter(){
+	var cnt = map.getCenter();
+  marker.setLatLng(cnt);
+  $('#latitude').val(cnt.lat);
+	$('#longitude').val(cnt.lng);
+	$('#legend')[0].innerHTML = convertDMS(cnt.lat,cnt.lng);
+}
+
+function iniMap(map){
+	map.options.scrollWheelZoom = 'center';
+  map.options.doubleClickZoom = 'center';
+  map.options.touchZoom = 'center';
+
+  var legend = L.control({position: 'bottomleft'});
+
+	legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend leaflet-control-attribution');
+    div.innerHTML = "<div id='legend'>-</div>";
+
+    return div;
+	};
+
+	legend.addTo(map);
+
+}
+
+function convertDMS( lat, lng ) {
+ 
+  var convertLat = Math.abs(lat);
+  var LatDeg = Math.floor(convertLat);
+  var LatSeg = (convertLat - LatDeg) * 3600
+  var LatMin = Math.floor(LatSeg / 60);
+  LatSeg = Math.floor(LatSeg - (LatMin * 60));
+  var LatCardinal = ((lat > 0) ? "N" : "S");
+   
+  var convertLng = Math.abs(lng);
+  var LngDeg = Math.floor(convertLng);
+  var LngSeg = (convertLng - LngDeg) * 3600
+  var LngMin = Math.floor(LngSeg / 60);
+  LngSeg = Math.floor(LngSeg - (LngMin * 60));
+  var LngCardinal = ((lng > 0) ? "E" : "W");
+   
+  return LatDeg + 'º' + LatMin + '′' + LatSeg + '″' + LatCardinal   + " " + LngDeg + 'º' + LngMin + '′' + LngSeg + '″' + LngCardinal;
+}
