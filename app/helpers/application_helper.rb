f826ebc36e355b9b90c5c0d998000f193e545e4b
devgarcia <tesdeve@gmail.com>
2019-05-28 09:56:21 +0100

    Navbar links style added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,14 @@ module ApplicationHelper
 			[0,0]
 		end
 	end
+
+	# Adds active status on navbar-links 
+	def link_to_in_li(body, url, html_options = {})
+	  active = "active" if current_page?(url)
+	  content_tag :li, class: active do
+	  link_to body, url, html_options
+	  end
+	end
+
+
 end
