f36421646e62b66088ab3866af66ffc39a581468
devgarcia <tesdeve@gmail.com>
2019-09-02 14:06:13 +0100

    Sender can choose to request the courier to come back to sending point or Not

---
+++ app/assets/javascripts/orders_return_show_hide.js
@@ -0,0 +1,45 @@
+(function($) {
+  document.addEventListener("turbolinks:load", function() {
+    var address0Value = $('#address0').val()
+    var yesChecked = $('input[name="order[return]"]:checked').val()
+    var returnTrue = document.getElementById('order_return_true')
+    var returnFalse = document.getElementById('order_return_false')
+    var topCardh3 = $('.top-card h3').text()
+
+    var latitude0 = $('#latitude0').val()
+    var longitude0 = $('#longitude0').val()
+
+    function comeBack() {
+      $('.card-right').hide() 
+      if ( topCardh3 == "Detalles de la Orden"){
+        $('.card-body-left h3').html("Dirección de Recogida y Entrega") 
+      } else {
+        $('.card-body-left h3').html("Sending and Receiving Location")
+      } 
+      $('.maps-row .col-md-6').removeClass( 'col-md-6' ).addClass( 'col-md-12' )
+      $('#address1').val(address0Value)
+      $('#latitude1').val(latitude0)
+      $('#longitude1').val(longitude0)
+      $(".btn-address-search").click()
+    }
+
+    function comeBackNot() {
+      $('.card-right').show()  
+      if ( topCardh3 == "Detalles de la Orden"){
+        $('.card-body-left h3').html("Direción de Recogida") 
+      } else {
+      $('.card-body-left h3').html("Sending Location")  
+      }
+      $('.maps-row .col-md-12').removeClass( 'col-md-12' ).addClass( 'col-md-6' ) 
+    }
+
+    if (yesChecked == "true"){
+      comeBack()
+    } else {
+      comeBackNot()
+    }
+
+    returnTrue.addEventListener('click', comeBack);
+    returnFalse.addEventListener('click', comeBackNot);
+  })
+})(jQuery)
